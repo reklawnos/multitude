1ef75e0e9c8272a67a0142a36a61925e4ac161c9
Walker Henderson <wjahenderson@gmail.com>
2018-05-22 00:32:16 -0700

    Much better second take

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export default function () {
-    console.log('test');
-}
-//# sourceMappingURL=index.js.map
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,24 +1,88 @@
-function createContexts<T extends object>(
+interface ContextValue<T> {
+  context: T;
+  contextNames: {
+    [K in keyof T]: string;
+  };
+}
+
+interface ContextsIterator<T> {
+  [Symbol.iterator]: () => {
+    next: () => {
+      done: boolean,
+      value: ContextValue<T>,
+    };
+  };
+}
+
+function createContextsIterator<T extends object>(
   contextGenerators: {
     [ContextKey in keyof T]: { [contextName: string]: () => T[ContextKey] };
   },
-) {
-  const result: { [k: string]: any } = {};
+): ContextsIterator<T> {
+  const contextGeneratorLists = Object.entries(contextGenerators)
+    .map(([key, value]) => [
+      key, Object.entries(value),
+    ])
+    .filter(([key, value]) => value.length > 0) as [string, [string, any[]]][];
 
-  Object.entries(contextGenerators).forEach(([key, value]) => {
-    const contextValues: { [k: string]: any } = {};
+  const valueFromIndex = (index: number) => {
+    const outputContext: { [k: string]: any } = {};
+    const outputContextNames: { [k: string]: string } = {};
 
-    Object.entries(value).forEach(([contextName, func]) => {
-      contextValues[contextName] = func();
+    let lenProduct = 1;
+    contextGeneratorLists.forEach(([key, options], i) => {
+      const [contextName, contextGenerator] = options[Math.floor(index / lenProduct) % options.length];
+      lenProduct *= options.length;
+      outputContext[key] = contextGenerator();
+      outputContextNames[key] = contextName;
     });
 
-    result[key] = contextValues;
-  });
+    return {
+      context: outputContext,
+      contextNames: outputContextNames,
+    } as ContextValue<T>;
+  };
 
-  return result as ;
+  const numItems = contextGeneratorLists.reduce((product, [key, options]) => product * options.length, 1);
+
+  const getIterator = () => {
+    const contextGeneratorIndices = contextGeneratorLists.map(() => 0);
+
+    let index = 0;
+
+    const next = () => {
+      if (index >= numItems) {
+        return {
+          done: true,
+          value: (undefined as any) as {
+            context: T;
+            contextNames: {
+              [K in keyof T]: string;
+            };
+          },
+        };
+      }
+      const res = {
+        value: valueFromIndex(index),
+        done: false,
+      };
+
+      index += 1;
+
+      return res;
+    };
+
+    return {
+      next,
+    };
+  };
+
+  return {
+    [Symbol.iterator]: getIterator,
+  };
 }
 
-const contexts = createContexts({
+const contexts = createContextsIterator({
   color: {
     'red': () => 'red',
     'blue': () => 'blue',
@@ -35,8 +99,45 @@ const contexts = createContexts({
     'not italic': () => false,
   },
 });
-console.log(contexts);
 
+function brokenThing(color: string, bold: boolean, italic: boolean): string {
+  if (italic) {
+    return 'red';
+  }
+  return color;
+}
+
+function runInContexts<T>(contextsIterator: ContextsIterator<T>, spec: (context: T) => void) {
+  const caughtErrors: [Error, {}][] = [];
+  for (let value of contextsIterator) {
+    try {
+      spec(value.context);
+    } catch(e) {
+      caughtErrors.push([e, value.contextNames]);
+    }
+  }
+  if (caughtErrors.length > 0) {
+    let out = '';
+    caughtErrors.forEach(([e, contextNames]) => {
+      out += 'When:\n';
+      const lines = Object.entries(contextNames)
+        .map(([key, name]) => `• ${key} is ${name}`);
+      out += lines.join('\n') + '\n\n';
+      out += e.message + '\n\n\n';
+    });
+    throw new Error(out);
+  }
+}
+
+it('does a thing', () => {
+  runInContexts(contexts, (context) => {
+    const { color, bold, italic } = context;
+    expect(brokenThing(color, bold, italic)).toBe(color);
+  });
+});
+
+
+/*
 interface ErrorInContext<T> {
   error: Error;
   context: T;
@@ -114,3 +215,5 @@ it ('it has a thing', () => {
     expect(brokenThing(context.color, context.bold, context.italic)).toBe(context.color);
   });
 });
+
+*/
